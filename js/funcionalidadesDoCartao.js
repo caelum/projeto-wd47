1a44bd1f64447636714d45885c51a7885865ccc9
Artur Diniz Adam <artur.daz@gmail.com>
2017-07-17 16:29:50 -0300

    IIFE

--- js/funcionalidadesDoCartao.js
+++ js/funcionalidadesDoCartao.js
@@ -1,10 +1,13 @@
-// Seleciona Elemento
-const btnRemove = document.querySelector('.opcoesDoCartao-remove')
-// Adiciona um Evento de click sem usar onclick e criando função anônima
-btnRemove.addEventListener('click', function() {
-	btnRemove.parentNode.parentNode.remove()
-})
+;(function(){
+	// Seleciona Elemento
+	const btn = document.querySelector('.opcoesDoCartao-remove')
+	// Adiciona um Evento de click sem usar onclick e criando função anônima
+	btn.addEventListener('click', function() {
+		btn.parentNode.parentNode.remove()
+	})
 
-// Falar para a galera testar as funcionalidades feitas até agora para dar o efeito de mudar o botão;
-// Usar let para poder sobreescrever o valor e dai então falar do const (const pode gerar dúvidas na hora da IIFE)
-btnRemove.classList.remove('no-js')
+	// Falar para btnRemovea galera testar as funcionalidades feitas até agora para dar o efeito de mudar o botão;
+	// Usar let para poder sobreescrever o valor e dai então falar do const (const pode gerar dúvidas na hora da IIFE)
+	btn.classList.remove('no-js')
+})()
+
--- js/mudaLayout.js
+++ js/mudaLayout.js
@@ -1,22 +1,21 @@
-(function() {
+;(function() {
+	// Ao clicarmos no botaoMudaLayout que possui o onclick atribuido
+	const mural = document.querySelector('.mural')
+	const btn = document.querySelector('#btnMudaLayout')
+	// Se 
+	function mudaLayout() {
+		mural.classList.toggle('mural--linha')
 
-// Ao clicarmos no botaoMudaLayout que possui o onclick atribuido
-const mural = document.querySelector('.mural')
-const btn = document.querySelector('#btnMudaLayout')
-// Se 
-function mudaLayout() {
-	mural.classList.toggle('mural--linha')
-
-	if (mural.classList.contains('mural--linha')) {
-		btn.textContent = 'Bloco'
-	} else {
-		btn.textContent = 'Linhas'
+		if (mural.classList.contains('mural--linha')) {
+			btn.textContent = 'Bloco'
+		} else {
+			btn.textContent = 'Linhas'
+		}
 	}
-}
 
-btn.addEventListener('click', mudaLayout)
+	btn.addEventListener('click', mudaLayout)
 
 
-btn.classList.remove('no-js')
+	btn.classList.remove('no-js')
 
 })()
