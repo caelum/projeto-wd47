eaff6b5bb9772c8a7ac6acba1113513c756f79d5
Artur Diniz Adam <artur.daz@gmail.com>
2017-07-17 16:47:05 -0300

    animacao da removacao

--- js/funcionalidadesDoCartao.js
+++ js/funcionalidadesDoCartao.js
@@ -3,9 +3,12 @@
 	const btns = document.querySelectorAll('.opcoesDoCartao-remove')
 	// Adiciona um Evento de click sem usar onclick e criando função anônima
 	for(let i = 0; i < btns.length; i++) {
-
 		btns[i].addEventListener('click', function() {
-			btns[i].parentNode.parentNode.remove()
+			const cartao = btns[i].parentNode.parentNode
+			cartao.classList.add("cartao--some")
+			cartao.addEventListener("transitionend", function(){
+				cartao.remove()
+			})
 		})
 		// Remove o no-js de todos os botões
 		btns[i].classList.remove('no-js')
