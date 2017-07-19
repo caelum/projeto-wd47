e9619c1d3d46261f0d2f2e14ac9267a19e80bfcf
Mario Souto <soutomarios@gmail.com>
2017-07-18 21:56:18 -0300

    Alteração no HTML para diminuir os loops e atribuição do event delegate para troca dos cartões, via click e via teclado

--- js/funcionalidadesDoCartao.js
+++ js/funcionalidadesDoCartao.js
@@ -1,5 +1,5 @@
 ;(function(){
-	// Seleciona Elemento
+	// Seleciona Elemento para aplicar a função de remover
 	const btns = document.querySelectorAll('.opcoesDoCartao-remove')
 	// Adiciona um Evento de click sem usar onclick e criando função anônima
 	for(let i = 0; i < btns.length; i++) {
@@ -10,42 +10,66 @@
 				cartao.remove()
 			})
 		})
-		// Remove o no-js de todos os botões
+		// Remove o no-js de todos os botões de remover
 		btns[i].classList.remove('no-js')
 	}
 
+	// - Pega todos os Cartões
 	const cartoes = document.querySelectorAll(".cartao")
-
+	// Funções dos Cartões
 	for(let j = 0; j < cartoes.length; j++){
 		const cartao = cartoes[j]
+		const tiposDoCartao = cartao.querySelector('.opcoesDoCartao-tipos')
 
+		// Navegação via teclado
 		cartao.addEventListener("focusin", function(){
 			cartao.classList.add("cartao--focado")
 		})
-
 		cartao.addEventListener("focusout", function(){
 			cartao.classList.remove("cartao--focado")
 		})
-	}
 
 
-	const tipoRadio = document.querySelectorAll(".opcoesDoCartao-radioTipo")
-	const tipoLabel = document.querySelectorAll(".opcoesDoCartao-tipo")
 
-	for(let k = 0; k < tipoRadio.length; k++){
-		const radio = tipoRadio[k];
-		const label = tipoLabel[k];
-
-		radio.addEventListener("onchange", function(event){
-			console.log(event.target.value)
-		})
-		label.classList.remove("no-js")
-		label.addEventListener("keydown", function(event){
+		// Habilita o click para as labels
+		tiposDoCartao.addEventListener("keydown", function(event){
+			console.log(event.target)
 			if(event.key === "Enter" || event.key === " "){
 				event.target.click()
 			}
 		})
+		// Event delegation do click do cartao
+		cartao.querySelector('.opcoesDoCartao').addEventListener('click', function() {
+			var elementoClicado = event.target
+
+			// Clicou nas cores
+			if(elementoClicado.className.match('opcoesDoCartao-radioTipo')) {
+				const corSelecionada = elementoClicado.value
+				cartao.style.backgroundColor = corSelecionada
+			}
+
+		})
+
+		// Removendo a class no-js da opcao de mudar tipo
+		tiposDoCartao.classList.remove('no-js')
 	}
 
-})()
+	// const tipoRadio = document.querySelectorAll(".opcoesDoCartao-radioTipo")
+	// const tipoLabel = document.querySelectorAll(".opcoesDoCartao-tipo")
+	//
+	// for(let k = 0; k < tipoRadio.length; k++){
+	// 	const radio = tipoRadio[k];
+	// 	const label = tipoLabel[k];
+	//
+	// 	radio.addEventListener("onchange", function(event){
+	// 		console.log(event.target.value)
+	// 	})
+		// label.classList.remove("no-js")
+	// 	label.addEventListener("keydown", function(event){
+	// 		if(event.key === "Enter" || event.key === " "){
+	// 			event.target.click()
+	// 		}
+	// 	})
+	// }
 
+})()
