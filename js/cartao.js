fa8b42ee2ae53d837bc508831ff7539ba5e017be
Artur Diniz Adam <artur.daz@gmail.com>
2017-07-21 19:36:40 -0300

    tirando caret do conteudo do cartao pq o chrome não tira (bug)

--- js/cartao.js
+++ js/cartao.js
@@ -37,7 +37,8 @@
 		})
 
 		cartao.addEventListener("keydown", function deixaClicarComEnter(event){
-			if(event.key === "Enter" || event.key === " "){
+			if(event.target.classList.contains("opcoesDoCartao-opcao") && event.key === "Enter" || event.key === " "){
+				window.getSelection().removeAllRanges()
 				event.target.click()
 			}
 		})
