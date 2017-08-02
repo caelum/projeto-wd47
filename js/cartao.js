3fd7752f6c042abcbe4fcbc2c2bda29f84a22a46
Mario Souto <soutomarios@gmail.com>
2017-08-02 16:43:01 -0300

    Remove cartão adicionado no delegate e explicação validada nessa segunda parte.

--- js/cartao.js
+++ js/cartao.js
@@ -1,18 +1,5 @@
 ;(function(){
 
-	//Funcionalidade remove cartão
-	const btns = document.querySelectorAll('.opcoesDoCartao-remove')
-	
-	for(let i = 0; i < btns.length; i++) {
-		btns[i].addEventListener('click', function() {
-			const cartao = btns[i].parentNode.parentNode
-			cartao.classList.add("cartao--some")
-			cartao.addEventListener("transitionend", function(){
-				cartao.remove()
-			})
-		})
-	}
-
 	const cartoes = document.querySelectorAll(".cartao")
 
 	for(let j = 0; j < cartoes.length; j++){
@@ -27,13 +14,19 @@
 		})
 
 		// Funcionalidade muda cor dos cartões
-		
 		cartao.addEventListener("click", function mudaCor(event){
 			const elementoSelecionado = event.target
 			const isRadioTipo = elementoSelecionado.classList.contains('opcoesDoCartao-radioTipo')
 			if(isRadioTipo) {
 				cartao.style.backgroundColor = elementoSelecionado.value
 			}
+			
+			if(elementoSelecionado.classList.contains('opcoesDoCartao-remove')){
+				cartao.classList.add("cartao--some")
+				cartao.addEventListener("transitionend", function(){
+					cartao.remove()
+				})
+			}	
 		})
 
 		cartao.addEventListener("keydown", function deixaClicarComEnter(event){
