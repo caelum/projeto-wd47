ba6d500a2a6cb2a0334805663fc0c1b1de0c80ce
Artur Diniz Adam <artur.daz@gmail.com>
2017-08-02 16:06:00 -0300

    cartao.js como estava no delegate

--- js/cartao.js
+++ js/cartao.js
@@ -1,64 +1,33 @@
 ;(function(){
-	const mural = document.querySelector(".mural")
-	let numeroDoCartao = 0
 
-	window.adicionaCartaoNoMural = function(conteudoDoCartao){
-		numeroDoCartao++
-
-		const templateCartao = document.createElement("tpl")
-
-		//declarativo
-		templateCartao.innerHTML = 
-		`<article id="cartao_${numeroDoCartao}" tabindex="0" class="cartao">
-			<div class="opcoesDoCartao">
-				<button class="opcoesDoCartao-remove opcoesDoCartao-opcao" tabindex="0">
-					<svg><use xlink:href="#iconeRemover"></use></svg>
-				</button>
-
-				<input type="radio" name="corDoCartao${numeroDoCartao}" value="#EBEF40" id="corPadrão-cartao${numeroDoCartao}" class="opcoesDoCartao-radioTipo">
-				<label for="corPadrão-cartao${numeroDoCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #EBEF40;" tabindex="0">
-					Padrão
-				</label>
-
-				<input type="radio" name="corDoCartao${numeroDoCartao}" value="#F05450" id="corImportante-cartao${numeroDoCartao}" class="opcoesDoCartao-radioTipo">
-				<label for="corImportante-cartao${numeroDoCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #F05450;" tabindex="0">
-					Importante
-				</label>
-
-				<input type="radio" name="corDoCartao${numeroDoCartao}" value="#92C4EC" id="corTarefa-cartao${numeroDoCartao}" class="opcoesDoCartao-radioTipo">
-				<label for="corTarefa-cartao${numeroDoCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #92C4EC;" tabindex="0">
-					Tarefa
-				</label>
-
-				<input type="radio" name="corDoCartao${numeroDoCartao}" value="#76EF40" id="corInspiração-cartao${numeroDoCartao}" class="opcoesDoCartao-radioTipo">
-				<label for="corInspiração-cartao${numeroDoCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #76EF40;" tabindex="0">
-					Inspiração
-				</label>
-			</div>
-			<p class="cartao-conteudo" contenteditable tabindex="0">${conteudoDoCartao}</p>
-		</article>`
-
-		const cartao = templateCartao.querySelector("article")
-
-		cartao.addEventListener('click', function() {
-			if(event.target.classList.contains("opcoesDoCartao-remove")){
-				cartao.classList.add("cartao--some")
-				cartao.addEventListener("transitionend", function(){
-					cartao.remove()
-				})
-			}
+	//Funcionalidade remove cartão
+	const btns = document.querySelectorAll('.opcoesDoCartao-remove')
+	
+	for(let i = 0; i < btns.length; i++) {
+		btns[i].addEventListener('click', function() {
+			const cartao = btns[i].parentNode.parentNode
+			cartao.classList.add("cartao--some")
+			cartao.addEventListener("transitionend", function(){
+				cartao.remove()
+			})
 		})
+	}
+
+	const cartoes = document.querySelectorAll(".cartao")
+
+	for(let j = 0; j < cartoes.length; j++){
+		const cartao = cartoes[j]
 
 		// Navegação com focus via teclado nos cartões
 		cartao.addEventListener("focusin", function(){
 			cartao.classList.add("cartao--focado")
 		})
-
 		cartao.addEventListener("focusout", function(){
 			cartao.classList.remove("cartao--focado")
 		})
 
-		// Funcionalidade muda cor dos cartões		
+		// Funcionalidade muda cor dos cartões
+		
 		cartao.addEventListener("click", function mudaCor(event){
 			const elementoSelecionado = event.target
 			const isRadioTipo = elementoSelecionado.classList.contains('opcoesDoCartao-radioTipo')
@@ -73,9 +42,6 @@
 				event.target.click()
 			}
 		})
-
-		mural.appendChild(cartao)
-		cartao.focus()
 	}
 
 })()
