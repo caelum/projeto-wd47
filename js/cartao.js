09e9d34a3fcace18a6dd69e944a87b5351ff7e11
Mario Souto <soutomarios@gmail.com>
2017-08-02 16:46:47 -0300

    Refatorada a função muda cor para mostrar o evento de change,  e trazer o click só para o remove

--- js/cartao.js
+++ js/cartao.js
@@ -14,13 +14,17 @@
 		})
 
 		// Funcionalidade muda cor dos cartões
-		cartao.addEventListener("click", function mudaCor(event){
+		cartao.addEventListener("change", function mudaCor(event){
 			const elementoSelecionado = event.target
 			const isRadioTipo = elementoSelecionado.classList.contains('opcoesDoCartao-radioTipo')
 			if(isRadioTipo) {
 				cartao.style.backgroundColor = elementoSelecionado.value
 			}
-			
+		})
+
+		//Funcionalidade remove cartão
+		cartao.addEventListener('click', function(event) {
+			const elementoSelecionado = event.target
 			if(elementoSelecionado.classList.contains('opcoesDoCartao-remove')){
 				cartao.classList.add("cartao--some")
 				cartao.addEventListener("transitionend", function(){
