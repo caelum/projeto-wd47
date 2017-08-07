c9ef937dc9eddd8371902a876b1c7e3ff10cecc4
Artur Diniz Adam <artur.daz@gmail.com>
2017-08-07 18:43:04 -0300

    forEach nas ajudas estaticas

--- js/opcoesDaPagina.js
+++ js/opcoesDaPagina.js
@@ -2,6 +2,7 @@
 	// Ao clicarmos no botaoMudaLayout que possui o onclick atribuido
 	const mural = document.querySelector('.mural')
 	const btn = document.querySelector('#btnMudaLayout')
+
 	// Se 
 	function mudaLayout() {
 		mural.classList.toggle('mural--linha')
@@ -13,8 +14,22 @@
 		}
 	}
 
-	btn.addEventListener('click', mudaLayout)
-	
+	btn.addEventListener('click', mudaLayout)	
 	btn.classList.remove('no-js')
+
+	const btnAjuda = document.querySelector('#btnAjuda')
+	btnAjuda.addEventListener("click", function(){
+		const ajudas = [
+			"Bem Vindo ao Ceep"
+			,"Clique no btn Linhas para mudar o layout"
+		]
+		
+
+		ajudas.forEach(function(ajuda){
+			alert(ajuda)
+		})
+		
+	})
+	btnAjuda.classList.remove('no-js')
 
 })()
