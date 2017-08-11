7a5ab83866b6429c4fe787a0d636c0b04a9f05df
Artur Diniz Adam <artur.daz@gmail.com>
2017-08-11 20:54:25 -0300

    usando babel cli (na aula nao teremos node scripts)

--- dist/js/opcoesDaPagina.js
+++ dist/js/opcoesDaPagina.js
@@ -1,34 +1,32 @@
-;(function() {
+'use strict';
+
+;(function () {
 	// Ao clicarmos no botaoMudaLayout que possui o onclick atribuido
-	const mural = document.querySelector('.mural')
-	const btn = document.querySelector('#btnMudaLayout')
+	var mural = document.querySelector('.mural');
+	var btn = document.querySelector('#btnMudaLayout');
 
 	// Se 
 	function mudaLayout() {
-		mural.classList.toggle('mural--linha')
+		mural.classList.toggle('mural--linha');
 
 		if (mural.classList.contains('mural--linha')) {
-			btn.textContent = 'Bloco'
+			btn.textContent = 'Bloco';
 		} else {
-			btn.textContent = 'Linhas'
+			btn.textContent = 'Linhas';
 		}
 	}
 
-	btn.addEventListener('click', mudaLayout)	
-	btn.classList.remove('no-js')
+	btn.addEventListener('click', mudaLayout);
+	btn.classList.remove('no-js');
 
-	const btnAjuda = document.querySelector('#btnAjuda')
-	btnAjuda.addEventListener("click", function(){
-		const ajudas = [
-			{conteudo: "Bem Vindo ao Ceep", cor: "#F05450"}
-			,{conteudo: "Clique no botão Linhas para alterar o layout", cor: "#92C4EC"}
-		]		
+	var btnAjuda = document.querySelector('#btnAjuda');
+	btnAjuda.addEventListener("click", function () {
+		var ajudas = [{ conteudo: "Bem Vindo ao Ceep", cor: "#F05450" }, { conteudo: "Clique no botão Linhas para alterar o layout", cor: "#92C4EC" }];
 
-		ajudas.forEach(function(ajuda){
-			adicionaCartaoNoMural(ajuda)
-		})		
-	})
-	
-	btnAjuda.classList.remove('no-js')
+		ajudas.forEach(function (ajuda) {
+			adicionaCartaoNoMural(ajuda);
+		});
+	});
 
-})()
+	btnAjuda.classList.remove('no-js');
+})();
