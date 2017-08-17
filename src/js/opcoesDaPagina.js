2e2861d149175cc57feb2672eec8655f3cdf032d
Artur Diniz Adam <artur.daz@gmail.com>
2017-08-17 19:22:00 -0300

    insrtucoes com ajax

--- src/js/opcoesDaPagina.js
+++ src/js/opcoesDaPagina.js
@@ -18,14 +18,18 @@
 	btn.classList.remove('no-js')
 
 	const btnAjuda = document.querySelector('#btnAjuda')
-	btnAjuda.addEventListener("click", function(){
-		const ajudas = [
-			{conteudo: "Bem Vindo ao Ceep", cor: "#F05450"}
-			,{conteudo: "Clique no botão Linhas para alterar o layout", cor: "#92C4EC"}
-		]		
-
-		ajudas.forEach(function(ajuda){
-			adicionaCartaoNoMural(ajuda)
+	btnAjuda.addEventListener("click", function(){	
+		const pegaAjudas = new XMLHttpRequest()
+		pegaAjudas.open('GET', 'https://ceep.herokuapp.com/cartoes/instrucoes')
+		pegaAjudas.responseType = "json"
+		pegaAjudas.send()
+		pegaAjudas.addEventListener("load", function(){
+			const objeto = pegaAjudas.response
+			const ajudas = objeto.instrucoes
+			
+			ajudas.forEach(function(ajuda){
+				adicionaCartaoNoMural(ajuda)
+			})	
 		})		
 	})
 	
