452a65287b8fbdf9661994a7fbe5dc126e5e9e08
Artur Diniz Adam <artur.daz@gmail.com>
2017-09-13 14:44:47 -0300

    Tirando nojs do remove

--- js/opcoesDoCartao/remove.js
+++ js/opcoesDoCartao/remove.js
@@ -4,7 +4,3 @@ const btnRemove = document.querySelector('.opcoesDoCartao-remove')
 btnRemove.addEventListener('click', function() {
 	btnRemove.parentNode.parentNode.remove()
 })
-
-// Falar para a galera testar as funcionalidades feitas até agora para dar o efeito de mudar o botão;
-// Usar let para poder sobreescrever o valor e dai então falar do const (const pode gerar dúvidas na hora da IIFE)
-btnRemove.classList.remove('no-js')
