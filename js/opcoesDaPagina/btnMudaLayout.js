93b406f1a7da0f91aea1a7c64524a8b0b7c1b3f3
Mario Souto <soutomarios@gmail.com>
2017-09-28 17:12:03 -0300

    Ajustando a ordem dos bagulhos do muda layout

--- js/opcoesDaPagina/btnMudaLayout.js
+++ js/opcoesDaPagina/btnMudaLayout.js
@@ -1,9 +1,11 @@
 const btn = document.querySelector('#btnMudaLayout') 
 
-btn.onclick = function mudaTexto() {
-    if(btn.textContent == 'Blocos') {
+function mudaTexto() {
+   if(btn.textContent == 'Blocos') {
         btn.textContent = 'Linhas'
     } else {
         btn.textContent = 'Blocos'
     }
 }
+
+btn.onclick = mudaTexto
