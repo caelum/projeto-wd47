0db261d09919a32f8e95507fe193693eadb7d1a1
Mario Souto <soutomarios@gmail.com>
2017-09-28 17:17:40 -0300

    evoluindo o mudaLayout

--- js/opcoesDaPagina/btnMudaLayout.js
+++ js/opcoesDaPagina/btnMudaLayout.js
@@ -1,11 +1,19 @@
 const btn = document.querySelector('#btnMudaLayout') 
 
 function mudaTexto() {
-   if(btn.textContent == 'Blocos') {
+    if(btn.textContent == 'Blocos') {
         btn.textContent = 'Linhas'
     } else {
         btn.textContent = 'Blocos'
     }
 }
 
-btn.onclick = mudaTexto
+btn.addEventListener('click', mudaTexto)
+
+const mural = document.querySelector('.mural')
+function mudaLayout() {
+    mural.classList.toggle('mural--linha')
+}
+
+btn.addEventListener('click', mudaLayout)
+
