150a96796bfe58574cc80fb59d22f8d0d0b5c285
Artur Diniz Adam <artur.daz@gmail.com>
2017-10-04 16:56:56 -0300

    erro de variaveis duplicadas sem IIFE

---
+++ js/opcoesDoCartao/remove.js
@@ -0,0 +1,10 @@
+// Seleciona Elemento
+const btn = document.querySelector('.opcoesDoCartao-remove')
+// Adiciona um Evento de click sem usar onclick e criando função anônima
+btn.addEventListener('click', function() {
+	const cartao = btn.parentNode.parentNode
+	cartao.classList.add("cartao--some")
+	cartao.addEventListener("transitionend", function(){
+		cartao.remove()
+	})
+})
